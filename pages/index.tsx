5dd741bd5730a146c53d40dda8563db130e48f5a
Muneeb Ahmad <muneeb.ahmad@pikessoft.com>
2022-07-26 01:42:34 +0500

    increased space between rows

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,7 @@ const Home = ({
       <main className=" relative pb-24 pl-4 lg:space-y-24 lg:pl-16">
         <Banner netflixOriginals={netflixOriginals} />
 
-        <section className="space-y-16">
+        <section className="md:space-y-24">
           <Row title="Trending Now" movies={trendingNow} />
           <Row title="Top Rated" movies={topRated} />
           <Row title="Action Movies" movies={actionMovies} />
