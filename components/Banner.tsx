2674b480e2a14062046c7f683020690a53205f84
Muneeb Ahmad <muneeb.ahmad@pikessoft.com>
2022-07-26 11:52:08 +0500

    added alt in images

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -22,6 +22,7 @@ const Banner = ({ netflixOriginals }: Props) => {
     <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[73vh] lg:justify-end lg:pb-12">
       <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
         <Image
+          alt="banner"
           src={`${BASE_URL}${movie?.backdrop_path || movie?.poster_path}`}
           layout="fill"
           objectFit="cover"
--- components/Thumbnails.tsx
+++ components/Thumbnails.tsx
@@ -11,6 +11,7 @@ const Thumbnails = ({ movie }: Props) => {
   return (
     <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-x-105">
       <Image
+        alt="thumbnail"
         src={`${BASE_URL}${movie?.backdrop_path || movie?.poster_path}`}
         className="rounded-sm object-cover md:rounded"
         layout="fill"
